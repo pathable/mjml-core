84b65acb3cec00ee3718d4db0736d6f93022d5b3
Renan Castro <renanccastro@gmail.com>
2019-09-06 16:13:22 -0300

    mjml-core

---
+++ src/components.js
@@ -0,0 +1,28 @@
+import kebabCase from 'lodash/kebabCase'
+
+const components = {}
+
+export function registerComponent(Component) {
+  components[kebabCase(Component.name)] = Component
+}
+
+export function initComponent({ initialDatas, name }) {
+  const Component = components[name]
+
+  if (Component) {
+    const component = new Component(initialDatas)
+
+    if (component.headStyle) {
+      component.context.addHeadSyle(name, component.headStyle)
+    }
+    if (component.componentHeadStyle) {
+      component.context.addComponentHeadSyle(component.componentHeadStyle)
+    }
+
+    return component
+  }
+
+  return null
+}
+
+export default components
